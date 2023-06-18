b971392ddcbd29c48faa615a1eff0a9014917a09
Boyeon Ihn <boyeonihn@gmail.com>
2023-06-18 13:55:54 +0900

    feat: add conditional rendering for longer summaries

--- src/Components/Movie.jsx
+++ src/Components/Movie.jsx
@@ -3,18 +3,12 @@ import { Link } from 'react-router-dom';
 
 export const Movie = ({ title, genres, coverImage, id, summary }) => {
   return (
-    <div>
-      <h2>
-        <Link to={`/movie/${id}`}>{title}</Link>
-      </h2>
-      <img src={coverImage} alt={title} />
-      <p>{summary.length > 233 ? `${summary.slice(0, 234)}...` : summary}</p>
-      <ul>
-        {genres
-          ? genres.map((genre, index) => <li key={index}>{`#${genre}`}</li>)
-          : ''}
-      </ul>
-    </div>
+          {summary.length > 233 ? `${summary.slice(0, 234)}...` : summary}{' '}
+          {summary.length > 233 ? (
+            <Link to={`/movie/${id}`}>see more.</Link>
+          ) : (
+            ''
+          )}
   );
 };
 

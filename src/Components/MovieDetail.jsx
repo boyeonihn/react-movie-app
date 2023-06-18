6b507d9a97580b4ed072e20625a7ae01ed62dec9
Boyeon Ihn <boyeonihn@gmail.com>
2023-06-18 14:20:31 +0900

    style: apply tailwind styling to MovieDetail Component

--- src/Components/MovieDetail.jsx
+++ src/Components/MovieDetail.jsx
@@ -10,13 +10,18 @@ export const MovieDetail = ({
   year,
 }) => {
   return (
-    <div>
-      <h2>{title}</h2>
-      <img src={coverImage} alt={title} />
-      <h3>Rating: {rating}</h3>
-      <h4>Released: {year}</h4>
-      <h4>Summary</h4>
-      <p>{summary}</p>
-    </div>
+    <main>
+      <Link className="font-medium" to={`/`}>
+        Go Back
+      </Link>
+      <section className="flex flex-col text-teal-700 bg-orange-100 p-8 rounded-lg">
+        <h1 className="font-semibold text-3xl">{title}</h1>
+        <img src={coverImage} alt={title} />
+        <h3>Rating: {rating}</h3>
+        <h4>Released: {year}</h4>
+        {summary ? <h4>Summary</h4> : ''}
+        <p>{summary}</p>
+      </section>
+    </main>
   );
 };
